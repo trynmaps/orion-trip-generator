2bcb3c0facc119c69f2e61e2348152580687c9e4
Hunter Stern <hunter.stern@autodesk.com>
2018-07-07 22:48:30 -0700

    Add more functionality

--- agencies/muni/muni.js
+++ agencies/muni/muni.js
@@ -1,73 +1,168 @@
 const awsHelper = require('../../awsHelper')
-const utils = require('../../util/utils')
 const muniConfig = require('./muniConfig');
 
-const getLatestVehicleDataAsTrips = async () => {
-  return await new Promise((resolve, reject) => {
-    awsHelper.listBucket(
-      { Bucket: muniConfig.vehicleBucket, Prefix: "muni-nextbus-bucket_" }).then((result) => {
-        let sortFn = (a, b) => {
-          let tsA = Number(a.Key.split("_")[1].split(".")[0])
-          let tsB = Number(b.Key.split("_")[1].split(".")[0])
+const updateTripState = (existingTrips, newTrips) => {
+  return new Promise((resolve, reject) => {
+    if (!existingTrips || existingTrips === undefined || existingTrips.length === 0) {
+      resolve([newTrips, [], []]);
+    }
 
-          if (tsA > tsB) {
-            return -1;
-          }
-          else {
-            return 1;
-          }
+    if (!newTrips || newTrips === undefined || newTrips.length === 0) {
+      resolve([[], existingTrips, []])
+    }
 
-          return 0;
+    const reducer = (acc, cur) => {
+      const index = newTrips.findIndex((n) => { return n.vid === cur.vid && n.route === cur.route && n.direction === cur.direction })
+      //should never be more than one match
+      if (index != -1) {
+        console.log(JSON.stringify(cur, null, 2))
+        const matchingTrip = newTrips[index]
+        for (let i = 0; i < matchingTrip.states.length; ++i) {
+          cur.states.push({
+            vtime: matchingTrip.states[i].vtime,
+            lat: matchingTrip.states[i].lat,
+            lon: matchingTrip.states[i].lon
+          })
         }
 
-        const mostRecentFile = result.sort(sortFn)[0].Key;
-        awsHelper.readTextS3(muniConfig.vehicleBucket, mostRecentFile).then((result) => {
-          const vehicleData = JSON.parse(result)
-          const tripStartTime = Date.now();
-          console.log("Loading trips from must recent muni trip file at s3://" + 
-            muniConfig.vehicleBucket + "/" + mostRecentFile);
-          resolve(vehicleData.map(v => {
-            return {
-              "agency": "muni",
-              "startTime": tripStartTime,
-              "endTime": null,
-              "route": v.routeId,
-              "direction": v.directionId,
-              "vid": v.id,
-              "states": [{
-                "vtime": tripStartTime,
-                "lat": v.lat,
-                "lon": v.lon
-              }]
-            }
-          }));
-        }).catch((err) => {
-          reject(err)
-        });
+        acc.updatedExistingTrips.push(cur)
+        //remove the matching trip from the array of new trips as it is not really new
+        newTrips.splice(index, 1);
+      }
+      else {
+        cur.endTime = Date.now()
+        acc.endedTrips.push(cur);
+      }
+
+      return acc;
+    };
+
+    const state = existingTrips.reduce(reducer, { updatedExistingTrips: [], endedTrips: [] })
+
+    resolve([newTrips, state.updatedExistingTrips, state.endedTrips])
+  })
+}
+
+const writeTrips = (trips) => {
+  let promises = [];
+
+  for (let i = 0; i < trips.length; ++i) {
+    promises.push(awsHelper.putFileToBucket({
+      Body: JSON.stringify(trips[i]),
+      Bucket: muniConfig.tripBucket,
+      Key: `muni-${trips[i].vid}_${trips[i].route}_${trips[i].direction}-${trips[i].startTime}-${trips[i].endTime}.json`
+    }))
+  }
+
+  return Promise.all(promises).then((results) => {
+    for (let i = 0; i < results.length; ++i) {
+      console.log("Uploaded state file to s3", results[i].status);
+    }
+
+    return results;
+  }).catch((err) => {
+    console.log("Error writing trip files", err)
+  })
+}
+
+const transformVehicleToTrip = (vehicle) => {
+  const tripStartTime = Date.now()
+  return {
+    agency: "muni",
+    startTime: tripStartTime,
+    endTime: null,
+    route: vehicle.routeId,
+    direction: vehicle.directionId,
+    vid: vehicle.id,
+    states: [{
+      vtime: tripStartTime,
+      lat: vehicle.lat,
+      lon: vehicle.lon
+    }]
+  }
+}
+
+const getVehicleDataAsTrips = (bucket, key) => {
+  return new Promise((resolve, reject) => {
+    if (bucket && key) {
+      awsHelper.readTextS3(bucket, key).then((result) => {
+        const vehicleData = JSON.parse(result)
+        console.log("Loading trips from most recent muni trip file at s3://" +
+          bucket + "/" + key);
+        resolve(vehicleData.map(transformVehicleToTrip));
+      }).catch((err) => {
+        reject(err)
       });
+    }
+    else {
+      awsHelper.listBucket(
+        { Bucket: muniConfig.vehicleBucket, Prefix: "muni-nextbus-bucket_" }).then((result) => {
+          let sortFn = (a, b) => {
+            let tsA = Number(a.Key.split("_")[1].split(".")[0])
+            let tsB = Number(b.Key.split("_")[1].split(".")[0])
+
+            if (tsA > tsB) {
+              return -1;
+            }
+            else {
+              return 1;
+            }
+
+            return 0;
+          }
+
+          const keys = result.sort(sortFn);
+
+          if (keys.length === 0) {
+            console.log("Could not find any vehicle data files in bucket s3://" + muniConfig.vehicleBucket)
+            return [];
+          }
+
+          const mostRecentFile = keys[0].Key;
+          awsHelper.readTextS3(muniConfig.vehicleBucket, mostRecentFile).then((result) => {
+            const vehicleData = JSON.parse(result)
+            console.log("Loading trips from most recent muni trip file at s3://" +
+              muniConfig.vehicleBucket + "/" + mostRecentFile);
+            resolve(vehicleData.map(transformVehicleToTrip));
+          }).catch((err) => {
+            reject(err)
+          });
+        });
+    }
   });
+}
+
+const writeTripStateFile = (trips) => {
+  return awsHelper.putFileToBucket({
+    Body: JSON.stringify(trips),
+    Bucket: muniConfig.stateBucket,
+    Key: muniConfig.stateFile
+  })
 }
 
 const initTripState = () => {
   return awsHelper.readTextS3(muniConfig.stateBucket, muniConfig.stateFile).then((data) => {
-    console.log("Loaded trip state from s3:", muniConfig.stateBucket, muniConfig.stateFile)
-    return data;
+    //first try to load state file
+    console.log("Loaded trip state from s3://" + muniConfig.stateBucket + "/" + muniConfig.stateFile)
+    return JSON.parse(data);
   }).catch((err) => {
-    return getLatestVehicleDataAsTrips();
-  }).then((trips) => {
-    awsHelper.putFileToBucket({
-      Body: new Buffer(trips, 'binary'),
-      Bucket: muniConfig.stateBucket,
-      Key: muniConfig.stateFile
-    }).then((result) => {
-      console.log("Uploaded state file to s3", result);
-    }).catch((err) => {
-      console.log("Error uploaded state file to s3", err);
-    })
+    //next try to get trips from latest muni vehicle data file
+    return getVehicleDataAsTrips().then((trips) => {
+      //create the state file
+      return writeTripStateFile(trips).then((result) => {
+        console.log("Uploaded state file to s3", result.status);
+        return JSON.parse(result.data);
+      }).catch((err) => {
+        console.log("Error uploaded state file to s3", err);
+      })
+    });
   });
 }
 
 module.exports = {
   initTripState: initTripState,
-  getLatestVehicleDataAsTrips: getLatestVehicleDataAsTrips
+  getVehicleDataAsTrips: getVehicleDataAsTrips,
+  writeTrips: writeTrips,
+  updateTripState: updateTripState,
+  writeTripStateFile: writeTripStateFile
 }
--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 const muni = require('./agencies/muni/muni');
 const sqsConsumer = require('sqs-consumer');
- 
+
 const app = sqsConsumer.create({
   queueUrl: 'https://sqs.us-west-2.amazonaws.com/140106082056/new-muni-vehicle-raw-data',
   handleMessage: (message, done) => {
-    console.log("handle message")
-    let body = JSON.parse(message.Body);
-    let messageDetail = JSON.parse(body.Message);
-    // console.log(JSON.stringify(messageDetail, null, 2));
+    const body = JSON.parse(message.Body);
+    const messageDetail = JSON.parse(body.Message);
 
     messageDetail.Records.map((record) => {
-      let bucket = record.s3.bucket.name;
-      let key = record.s3.object.key;
-
-      console.log(bucket);
-      console.log(key)
+      console.log("New vehicle file published in bucket", record.s3.bucket.name, "key", record.s3.object.key)
+      muni.getVehicleDataAsTrips(record.s3.bucket.name, record.s3.object.key).then((newTrips) => {
+        muni.updateTripState(app.trips, newTrips).then((result) => {
+          const newTrips = result[0];
+          const updatedTrips = result[1];
+          const endedTrips = result[2];
+          console.log(endedTrips.length)
+          muni.writeTrips(endedTrips).then((result) => {
+            console.log("Wrote", endedTrips.length, "new trips")
+            console.log("Write status is", result)
+            app.trips = updatedTrips.concat(newTrips);
+            muni.writeTripStateFile(app.trips).then((result) => {
+              console.log("Uploaded state file to s3", result.status);
+            }).catch((err) => {
+              console.log("Error uploaded state file to s3", err);
+            })
+          }).catch((err) => {
+            console.log("Error writing new trips", err)
+          })
+        }).catch((err) => {
+          console.log("Error updating trip state", err)
+        })
+      }).catch((err) => {
+        console.log("Error reading new vehicle data from s3://" + record.s3.bucket.name + "/" + record.s3.object.key)
+        console.log(err)
+      })
     })
-
     done();
   }
 });
 
-app.trips = [];
 app.on('error', (err) => {
   console.log(err.message);
 });
 
 muni.initTripState().then((trips) => {
+  app.trips = [];
   app.trips = trips;
-  //console.log("trips", JSON.stringify(app.trips, null, 2));
-  console.log("Loaded", app.trips.length, "trips on startup");  
+  console.log("Loaded", app.trips.length, "trips on startup");
   app.start();
 }).catch((err) => {
   console.log("error starting app", err);
